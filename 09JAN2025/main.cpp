578dc9dc2057f501a8a90ce55d3c4441626cbc10
Seyed Mahmoud Shahrokni <shahrokni.trivia@gmail.com>
2025-01-14 14:05:12 +0100

    refactor: Format c files using CLang formatter

--- 09JAN2025/main.cpp
+++ 09JAN2025/main.cpp
@@ -1,16 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
-    
-    for(int j=0;j<15;j++){
-        for(int i=0;i<15;i++)
-    cout<<" * ";
-    cout<<"\n";
+int main() {
 
-    }
+  for (int j = 0; j < 15; j++) {
+    for (int i = 0; i < 15; i++)
+      cout << " * ";
+    cout << "\n";
+  }
 
-    return 0;
+  return 0;
 }
-
