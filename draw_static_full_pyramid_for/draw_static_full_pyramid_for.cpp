9ae2fb6e775fe35c568e2c4cc040925bf39fb0a9
Reza <shahroknireza001@gmail.com>
2025-01-23 19:53:30 +0330

    feat: Add full pyramid with for loop

---
+++ draw_static_full_pyramid_for/draw_static_full_pyramid_for.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void draw_static_full_pyramid_for()
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+
+            cout << " * ";
+        }
+
+        cout << "\n";
+    }
+
+    cout<<" *  *  *  *  *  *  * \n";
+    
+
+    for (int i= 6; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " * ";
+        }
+
+        cout << "\n";
+    }
+}
--- main.cpp
+++ main.cpp
@@ -3,13 +3,15 @@
 #include "draw_dynamic_square_for/draw_dynamic_square_for.h"
 #include "draw_static_half_pyramid_for/draw_static_half_pyramid_for.h"
 #include "draw_static_half_pyramid_while/draw_static_half_pyramid_while.h"
+#include "draw_static_full_pyramid_for/draw_static_full_pyramid_for.h"
 #include <iostream>
 
 int main() {
   // draw_static_square();
   // draw_dynamic_square_for();
   // draw_static_half_pyramid_for();
-  draw_static_half_pyramid_while();
+ // draw_static_half_pyramid_while();
+ draw_static_full_pyramid_for();
   return 0;
   
 }
